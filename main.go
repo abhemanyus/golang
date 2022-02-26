05f4519315f2aeb9938d3d24b1ca246f748677f9
Abhemanyu Sarin <abhemanyus@gmail.com>
2022-02-26 16:40:29 +0530

    ouch

--- main.go
+++ main.go
@@ -1,35 +1,12 @@
 package main
 
-const (
-	ErrKeyNotFound  = DictionaryErr("key not found")
-	ErrDuplicateKey = DictionaryErr("duplicate key entry")
+import (
+	"fmt"
+	"io"
 )
 
-type DictionaryErr string
+const englishGreetingPrefix = "Hello, "
 
-func (err DictionaryErr) Error() string {
-	return string(err)
-}
-
-type Dictionary map[string]string
-
-func (d Dictionary) Search(key string) (string, error) {
-	value, ok := d[key]
-	if !ok {
-		return "", ErrKeyNotFound
-	}
-	return value, nil
-}
-
-func (d Dictionary) Add(key, value string) error {
-	_, ok := d[key]
-	if ok {
-		return ErrDuplicateKey
-	}
-	d[key] = value
-	return nil
-}
-
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+func Greet(w io.Writer, name string) {
+	fmt.Fprint(w, englishGreetingPrefix+name)
 }
--- main_test.go
+++ main_test.go
@@ -1,76 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
-func assertString(t testing.TB, want, got string) {
-	t.Helper()
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
 	if got != want {
 		t.Errorf("want %q, got %q", want, got)
 	}
-}
-func assertError(t testing.TB, want, got error) {
-	t.Helper()
-	if got != want {
-		t.Errorf("want %q, got %q", want, got)
-	}
-}
-
-func TestSearch(t *testing.T) {
-	t.Run("known word", func(t *testing.T) {
-		myMap := Dictionary{"test": "this is just a test"}
-		got, _ := myMap.Search("test")
-		want := "this is just a test"
-
-		assertString(t, want, got)
-	})
-	t.Run("unknown word", func(t *testing.T) {
-		myMap := Dictionary{"test": "this is just a test"}
-		_, err := myMap.Search("unknown")
-
-		assertError(t, ErrKeyNotFound, err)
-	})
-}
-
-func assertDefinition(t testing.TB, m Dictionary, key, value string) {
-	t.Helper()
-	got, err := m.Search(key)
-
-	if err != nil {
-		t.Fatal("should find added word: ", key)
-	}
-
-	if got != value {
-		t.Errorf("want %q, got %q, given %q", value, got, key)
-	}
-}
-
-func TestAdd(t *testing.T) {
-	t.Run("add new", func(t *testing.T) {
-		myMap := Dictionary{"test": "this is just a test"}
-		key, value := "cat", "this is a cat"
-		err := myMap.Add(key, value)
-		assertError(t, nil, err)
-		assertDefinition(t, myMap, key, value)
-	})
-
-	t.Run("add existing", func(t *testing.T) {
-		key, value := "test", "this is just a test"
-		myMap := Dictionary{"test": value}
-		err := myMap.Add(key, "this is a cat")
-		assertError(t, ErrDuplicateKey, err)
-		assertDefinition(t, myMap, key, value)
-	})
-}
-
-func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
-
-	dictionary.Update(word, newDefinition)
-
-	assertDefinition(t, dictionary, word, newDefinition)
 }
